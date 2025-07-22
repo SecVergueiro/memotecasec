29e0b817e59865d677684e9df0a13789532043b2
Isaque Vergueiro <isaquevergueironox@gmail.com>
2025-07-22 18:51:47 -0400

    Ao inicializar abrir o GIT BASH e por cd backend e depois npm start

---
+++ js/api.js
@@ -0,0 +1,59 @@
+const URL_BASE = "http://localhost:3000";
+
+const api = {
+  async buscarPensamentos() {
+    try {
+      const response = await axios.get(`${URL_BASE}/pensamentos`);
+      return response.data;
+    } catch (error) {
+      alert('Erro ao buscar pensamentos');
+      throw error;
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento);
+      return response.data;
+    } catch (error) {
+      alert('Erro ao salvar pensamento');
+      throw error;
+    }
+  },
+
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await axios.get(`${URL_BASE}/pensamentos/${id}`);
+      return response.data;
+    } catch (error) {
+      alert('Erro ao buscar pensamento');
+      throw error;
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await axios.put(
+        `${URL_BASE}/pensamentos/${pensamento.id}`,
+        pensamento
+      );
+      return response.data;
+    } catch (error) {
+      alert('Erro ao editar pensamento');
+      throw error;
+    }
+  },
+
+  async excluirPensamento(id) {
+    try {
+      await fetch(`${URL_BASE}/pensamentos/${id}`, {
+        method: "DELETE"
+      });
+    } catch (error) {
+      alert('Erro ao excluir um pensamento');
+      throw error;
+    }
+  }
+};
+
+export default api;
